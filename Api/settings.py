cbfcfc7ae0c22bdc5388a372cbb80c8ada90a7bc
MuhammadTaneem <muhammadtaneem.4@gmail.com>
2021-09-17 20:52:18 +0600

    update allowed hosts

--- Api/settings.py
+++ Api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -187,7 +187,10 @@ CORS_ORIGIN_WHITELIST = (
     BackEndUrl,
 
 )
-
+ALLOWED_HOSTS = [
+    FrontEndUrl,
+    BackEndUrl,
+]
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
