3e017cfdd7a9084d1e5ed743c3aca41189973d79
MuhammadTaneem <59029167+MuhammadTaneem@users.noreply.github.com>
2021-09-18 21:27:21 +0600

    Update settings.py
    whitelist change agin

--- Api/settings.py
+++ Api/settings.py
@@ -181,14 +181,16 @@ REST_FRAMEWORK = {
 
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
-# CORS_ORIGIN_WHITELIST = (
-#     FrontEndUrl,
-#     BacktEndUrl,
-#     # 'http://localhost:4200',
+CORS_ORIGIN_WHITELIST = (
+    FrontEndUrl,
+    BacktEndUrl,
+    'http://localhost:4200',
+    'http://localhost:8000',
+    'http://localhost:8080',
 
-# )
+)
 ALLOWED_HOSTS = [
    '127.0.0.1',
     BacktEndUrl
